c830c4c8f5268849010442aecf1eea60f3ae847a
Thyago Schleuss <thyago.schleuss@ada.support>
2022-05-05 11:30:52 -0500

    adding path mapper to jest and ts config files

--- jest.config.js
+++ jest.config.js
@@ -10,8 +10,8 @@ module.exports = {
   },
   preset: "ts-jest",
   testEnvironment: "node",
-  transform: {
-    "^.+\\.ts?$": "ts-jest",
-  },
   transformIgnorePatterns: ["<rootDir>/node_modules/"],
+  moduleNameMapper: {
+    "~/(.*)": "<rootDir>/src/$1",
+  },
 };
